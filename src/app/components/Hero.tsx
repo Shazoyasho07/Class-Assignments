5984dedbcfff9721efa93845e5cf39c519f992d9
Mrs.Yaseen <shahzeen.soomro1993@gmail.com>
2024-11-09 02:21:22 +0500

    Initial Commit

--- src/app/components/Hero.tsx
+++ src/app/components/Hero.tsx
@@ -1,19 +1,26 @@
-import React from "react";
-import Image from "next/image";
-import imagee from "../../../public/Imagee.png"
-const Hero = () => {
-    return (
-        <div className='flex items-center h-screen'>
-        <div className='flex flex-col justify-center w-1/2 gap-6 px-14'>
-            <h1 className='w-[548px] [h-51px] font-bold text-[44px] leading-[51.25px] text-black'>Welcome To Our Website</h1>
-            <p className='w-[571px] [h-156px] font-normal text-[32px] leading-[38.73px] text-[#4C4C4C]'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard</p>
-            <button className='w-[237px] [h-66px] bg-black text-white p-4 font-normal leading-[33.89px] text-[28px]'>Contact Us</button>
-        </div>
-        <div>
-            <Image src={imagee} alt="image" />
-        </div>
-        </div>
-    )
-}
+// import React from "react";
+// import Image from "next/image";
+// import imagee from "../../../public/Imagee.png"
+// const Hero = () => {
+//     return (
+//         <div className='flex items-center h-screen'>
+//             <div className='flex flex-col justify-center w-1/2 gap-6 px-14'>
+//                 <h1 className='w-[548px] [h-51px] font-bold text-[44px] leading-[51.25px] text-black'>
+//                     Welcome To Our Website
+//                 </h1>
+//                 <p className='w-[571px] [h-156px] font-normal text-[32px] leading-[38.73px] text-[#4C4C4C]'>
+//                     Lorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum has been the industry&apos;s standard.
+//                 </p>
+//                 <button className='w-[237px] [h-66px] bg-black text-white p-4 font-normal leading-[33.89px] text-[28px]'>
+//                     Contact Us
+//                 </button>
+//             </div>
+//             <div>
+//                 <Image src={imagee} alt="image" />
+//             </div>
+//         </div>
+//     );
+// };
 
-export default Hero
+// export default Hero;
+
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,27 @@
-import React from "react";
-import Hero from "./components/Hero";
-import Header from "./components/Header";
+// import React from "react";
+// import Hero from "./components/Hero";
+// import Header from "./components/Header";
 
-export default function home(){
-  return (
-  <div>
-    <Header />
-    <main>
-       <Hero /> 
-    </main>
-  </div>
-  );
-  }
+// export default function home(){
+//   return (
+//   <div>
+//     <Header />
+//     <main>
+//        <Hero /> 
+//     </main>
+//   </div>
+//   );
+//  }
+
+export default function Home() {
+    return (
+        <div className="grid grid-rows-4 grid-cols-3 gap-1 h-screen p-6">
+          <div className="col-span-3 setup bg-blue-400">Header</div>
+          <div className="row-span-2 setup bg-pink-400" >Sidebar</div>
+          <div className="col-span-2 setup bg-purple-400">Content-01</div>
+          <div className="col-span-1 setup bg-gray-400">Content-02</div>
+          <div className="col-span-1 setup bg-green-400">Content-03</div>
+          <div className="col-span-3 setup bg-blue-400">Footer</div>
+        </div>
+    );
+}
